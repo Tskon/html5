11ca0240ce01cc5fb03692a01a17b4aa16691d08
tskon <beatxd@gmail.com>
2017-10-22 00:59:30 +0300

    lesson4: contact form upgrade

--- firstTemplate/js/main.js
+++ firstTemplate/js/main.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-
     scaleVideoContainer();
 
     initBannerVideoSize('.video-container .poster img');
@@ -12,26 +11,20 @@ $( document ).ready(function() {
         scaleBannerVideoSize('.video-container .filter');
         scaleBannerVideoSize('.video-container video');
     });
-
 });
 
 function scaleVideoContainer() {
-
     var height = $('.filter').innerHeight();
     var unitHeight = parseInt(height) + 'px';
     $('.homepage-hero-module').css('height',unitHeight);
-
 }
 
 function initBannerVideoSize(element){
-
     $(element).each(function(){
         $(this).data('height', $(this).height());
         $(this).data('width', $(this).width());
     });
-
     scaleBannerVideoSize(element);
-
 }
 
 function scaleBannerVideoSize(element){
@@ -55,8 +48,6 @@ function scaleBannerVideoSize(element){
 
             $(this).width(videoWidth).height(videoHeight);
         }
-
         $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
     });
 }
