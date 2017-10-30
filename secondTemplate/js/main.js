4cb1859634ced818faf7d0cc9f06a2639fa8205c
tskon <beatxd@gmail.com>
2017-10-31 01:04:57 +0300

    lesson5(second tmpl): some js for darkness..

--- secondTemplate/js/main.js
+++ secondTemplate/js/main.js
@@ -1,17 +1,30 @@
 'use strict';
 // ideas animation start
 let ideaItemArr = document.querySelectorAll('.ideas__examples-item');
-let width = 0;
-let height = 0;
+let div;
+let timer;
 for (let i = 0; i < ideaItemArr.length; i++) {
   ideaItemArr[i].addEventListener('mouseover', function () {
-
-    if (this.classList.contains('ideas__example1')) {
-
+    for (let i = 0; i < ideaItemArr.length; i++) {
+      ideaItemArr[i].style.opacity = 0.7;
     }
+    this.style.opacity = 1;
+    div = document.createElement('div');
+    div.classList.add('transparent-hover');
+    document.body.appendChild(div);
+    setTimeout(function () {
+      clearInterval(timer);
+      div.classList.add('black-hover');
+    }, 0);
   });
   ideaItemArr[i].addEventListener('mouseout', function () {
-
+    div.classList.remove('black-hover');
+    for (let i = 0; i < ideaItemArr.length; i++) {
+      ideaItemArr[i].style.opacity = 1;
+    }
+    timer = setTimeout(function () {
+      div.parentNode.removeChild(div);
+    }, 200);
   });
 }
 // ideas animation end
