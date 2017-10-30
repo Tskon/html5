6724b73b38d6e0d65047631861ff4e301c6be827
tskon <beatxd@gmail.com>
2017-10-31 00:00:05 +0300

    lesson5(second tmpl): convert js to css =)

--- secondTemplate/js/main.js
+++ secondTemplate/js/main.js
@@ -3,31 +3,15 @@
 let ideaItemArr = document.querySelectorAll('.ideas__examples-item');
 let width = 0;
 let height = 0;
-for (let i = 0; i < ideaItemArr.length; i++){
-    ideaItemArr[i].addEventListener('mouseover', function(){
-        width = this.width;
-        height = this.height;
-        this.style.zIndex = 15;
-        if (this.classList.contains('ideas__example2')){
-            this.style.position = 'relative';
-            // this.style.top = -(height * 0.05) + 'px';
-            this.style.width = (width * 1.1) + 'px';
-            this.style.height = (height * 1.1) + 'px';
-        }else{
-            this.style.top = -(height * 0.10) + 'px';
-            this.style.width = (width * 1.3) + 'px';
-            this.style.height = (height * 1.3) + 'px';
-        }
-        if (this.classList.contains('ideas__example1')){
-            this.style.left = (height * 0.3) + 'px';
-        }
-    });
-    ideaItemArr[i].addEventListener('mouseout', function(){
-        this.style.zIndex = '';
-        this.style.width = '';
-        this.style.height = '';
-        this.style.top = '';
-        this.style.left = '';
-    });
+for (let i = 0; i < ideaItemArr.length; i++) {
+  ideaItemArr[i].addEventListener('mouseover', function () {
+
+    if (this.classList.contains('ideas__example1')) {
+
+    }
+  });
+  ideaItemArr[i].addEventListener('mouseout', function () {
+
+  });
 }
 // ideas animation end
