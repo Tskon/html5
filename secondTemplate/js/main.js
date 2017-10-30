d8ac85d616a8a40b6e82131491b56bc838d38995
tskon <beatxd@gmail.com>
2017-10-31 01:57:32 +0300

    lesson5(second tmpl): js - fix blink on slides

--- secondTemplate/js/main.js
+++ secondTemplate/js/main.js
@@ -2,29 +2,37 @@
 // ideas animation start
 let ideaItemArr = document.querySelectorAll('.ideas__examples-item');
 let div;
-let timer;
+let timer1;
+let timer2;
 for (let i = 0; i < ideaItemArr.length; i++) {
   ideaItemArr[i].addEventListener('mouseover', function () {
+    clearInterval(timer1);
+    clearInterval(timer2);
     for (let i = 0; i < ideaItemArr.length; i++) {
       ideaItemArr[i].style.opacity = 0.7;
     }
     this.style.opacity = 1;
-    div = document.createElement('div');
-    div.classList.add('transparent-hover');
-    document.body.appendChild(div);
-    setTimeout(function () {
-      clearInterval(timer);
-      div.classList.add('black-hover');
-    }, 0);
+    if (document.querySelector('.black-hover') == null) {
+      div = document.createElement('div');
+      div.classList.add('transparent-hover');
+      document.body.appendChild(div);
+      setTimeout(function () {
+        div.classList.add('black-hover');
+      }, 0);
+    }
   });
+
   ideaItemArr[i].addEventListener('mouseout', function () {
-    div.classList.remove('black-hover');
     for (let i = 0; i < ideaItemArr.length; i++) {
       ideaItemArr[i].style.opacity = 1;
     }
-    timer = setTimeout(function () {
+    timer1 = setTimeout(function () {
+      div.classList.remove('black-hover');
+    }, 20);
+
+    timer2 = setTimeout(function () {
       div.parentNode.removeChild(div);
-    }, 200);
+    }, 220);
   });
 }
 // ideas animation end
